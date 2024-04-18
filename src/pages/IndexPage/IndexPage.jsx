7d69f1e1583c54e24d2f71e6fe958727b234d269
jay ess <mentoce@gmail.com>
2024-04-17 21:45:47 -0700

    Commit to remove

--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import { Box, Tab, Tabs, Typography } from "@mui/material";
 import { BarChart } from "@mui/x-charts/BarChart";
+import { DataGrid } from "@mui/x-data-grid";
 
 const IndexPage = () => {
     const [currentTabIndex, setCurrentTabIndex] = useState(0);
@@ -17,6 +18,147 @@ const IndexPage = () => {
     const sizingProps = { width: 600, height: 500 };
     console.log(axis);
     console.log(sizingProps);
+
+    const tenantColumns = [
+        {
+            field: "rowTitle",
+            headerName: "Tenant Summary",
+            minWidth: 150,
+            editable: false,
+        },
+        {
+            field: "value1",
+            headerName: "Deal 1",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "value2",
+            headerName: "Deal 2",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "value3",
+            headerName: "Deal 3",
+            width: 150,
+            editable: false,
+        },
+    ];
+    const tenantRows = [
+        {
+            id: 0,
+            rowTitle: "Total Cost For Occupancy",
+            value1: "$621,792.69",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 1,
+            rowTitle: "Present Value",
+            value1: "$509,096.26",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 2,
+            rowTitle: "Net Effective Rate per Annum",
+            value1: "$112,542.82",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 3,
+            rowTitle: "Net Effective Rate per Month",
+            value1: "$9,378.57",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 4,
+            rowTitle: "Net Effective Rate per Annum/SF",
+            value1: "$40.28",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 5,
+            rowTitle: "Net Effective Rate per Month/SF",
+            value1: "$3.36",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+    ];
+    const ownerColumns = [
+        {
+            field: "rowTitle",
+            headerName: "Owner Summary",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "value1",
+            headerName: "Deal 1",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "value2",
+            headerName: "Deal 2",
+            width: 150,
+            editable: false,
+        },
+        {
+            field: "value3",
+            headerName: "Deal 3",
+            width: 150,
+            editable: false,
+        },
+    ];
+    const ownerRows = [
+        {
+            id: 0,
+            rowTitle: "Total Cost For Occupancy",
+            value1: "$621,792.69",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 1,
+            rowTitle: "Present Value",
+            value1: "$509,096.26",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 2,
+            rowTitle: "Net Effective Rate per Annum",
+            value1: "$112,542.82",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 3,
+            rowTitle: "Net Effective Rate per Month",
+            value1: "$9,378.57",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 4,
+            rowTitle: "Net Effective Rate per Annum/SF",
+            value1: "$40.28",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+        {
+            id: 5,
+            rowTitle: "Net Effective Rate per Month/SF",
+            value1: "$3.36",
+            value2: "$0.00",
+            value3: "$0.00",
+        },
+    ];
 
     return (
         <React.Fragment>
@@ -53,16 +195,50 @@ const IndexPage = () => {
 
             {/* Tab 2 contents */}
             {currentTabIndex === 1 && (
-                <Box sx={{ p: 3 }}>
-                    <Typography variant="h5">Tab 2 Content</Typography>
-                    <Typography variant="p">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book.
-                    </Typography>
-                </Box>
+                <>
+                    {/* <Box sx={{ m: 5, height: 20 }} /> */}
+                    <Box
+                        sx={{
+                            p: 3,
+                            height: 200,
+                            width: 500,
+                            marginLeft: 5,
+                            marginTop: 10,
+                        }}
+                    >
+                        <DataGrid
+                            rows={tenantRows}
+                            columns={tenantColumns}
+                            autosizeOptions={{
+                                columns: [
+                                    "Tenant Summary",
+                                    "Deal 1",
+                                    "Deal 2",
+                                    "Deal 3",
+                                ],
+                                includeOutliers: true,
+                                includeHeaders: false,
+                            }}
+                        />
+                    </Box>
+
+                    <Box sx={{ p: 3, height: 300, marginTop: 10 }}>
+                        <DataGrid
+                            rows={ownerRows}
+                            columns={ownerColumns}
+                            autosizeOptions={{
+                                columns: [
+                                    "Ten Summary",
+                                    "Deal 1",
+                                    "Deal 2",
+                                    "Deal 3",
+                                ],
+                                includeOutliers: true,
+                                includeHeaders: false,
+                            }}
+                        />
+                    </Box>
+                </>
             )}
 
             {/* Tab 3 contents */}
