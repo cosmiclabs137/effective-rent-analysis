5fc2738cdb023e40be1209c418dce5f8f4e8e284
jay ess <mentoce@gmail.com>
2024-05-08 14:57:31 -0700

    Add all inputs from spreadsheet

--- src/components/DealInputForm/DealInputForm.jsx
+++ src/components/DealInputForm/DealInputForm.jsx
@@ -27,8 +27,28 @@ const DealInputForm = ({
     handleTenantDiscountRate,
     landlordDiscountRate,
     handleLandlordDiscountRate,
-    commissionPercent,
-    handleCommissionPercent,
+    commissionPercent1st,
+    handleCommissionPercent1st,
+    growthRateInOpEx,
+    handleGrowthRateInOpEx,
+    otherRecurringCost,
+    handleOtherRecurringCost,
+    otherNonRecurringCost,
+    handleOtherNonRecurringCost,
+    recurringCostGrowthRate,
+    handleRecurringCostGrowthRate,
+    tenantImprovementCost,
+    handleTenantImprovementCost,
+    tenantImprovementAllowance,
+    handleTenantImprovementAllowance,
+    otherNonRecurringContribution,
+    handleOtherNonRecurringContribution,
+    otherRecurringContribution,
+    handleOtherRecurringContribution,
+    contributionGrowthRate,
+    handleContributionGrowthRate,
+    commissionPercent2nd,
+    handleCommissionPercent2nd,
     handleSubmit,
     xs = 3,
 }) => {
@@ -111,13 +131,157 @@ const DealInputForm = ({
                         onChange={(e) => handleOccupancyExpensesPsf(e)}
                     />
                     <TextField
+                        name="growth-rate-in-opex"
+                        label="Growth rate in OpEx (%)"
+                        helperText="Growth rate in operating expenses (%)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    %
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={growthRateInOpEx}
+                        onChange={(e) => handleGrowthRateInOpEx(e)}
+                    />
+                    <TextField
+                        name="other-non-recurring-cost"
+                        label="Other Non-Recurring Cost"
+                        helperText="Other Non-Recurring Cost ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={otherNonRecurringCost}
+                        onChange={(e) => handleOtherNonRecurringCost(e)}
+                    />
+                    <TextField
+                        name="other-recurring-cost"
+                        label="Other Recurring Cost (Total)"
+                        helperText="Other Recurring Cost ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={otherRecurringCost}
+                        onChange={(e) => handleOtherRecurringCost(e)}
+                    />
+                    <TextField
+                        name="recurring-cost-growth-rate"
+                        label="Recurring Cost Growth Rate"
+                        helperText="Recurring Cost Growth Rate (%)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    %
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={recurringCostGrowthRate}
+                        onChange={(e) => handleRecurringCostGrowthRate(e)}
+                    />
+                    <TextField
+                        name="tenant-improvement-cost"
+                        label="Tenant Improvement Cost"
+                        helperText="Tenant Improvement Cost ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={tenantImprovementCost}
+                        onChange={(e) => handleTenantImprovementCost(e)}
+                    />
+                    <TextField
+                        name="tenant-improvement-allowance"
+                        label="Tenant Improvement Allowance"
+                        helperText="Tenant Improvement Allowance ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={tenantImprovementAllowance}
+                        onChange={(e) => handleTenantImprovementAllowance(e)}
+                    />
+                    <TextField
                         name="months-free-rent"
                         label="Months free rent"
-                        helperText="The number of months with free rend"
+                        helperText="The number of months with free rent"
                         type="number"
                         variant="standard"
                         value={monthsFreeRent}
                         onChange={(e) => handleMonthsFreeRent(e)}
+                    />
+                    <TextField
+                        name="other-non-recurring-contribution"
+                        label="Other Non-Recurring Contribution (Total)"
+                        helperText="Other Non-Recurring Contribution ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={otherNonRecurringContribution}
+                        onChange={(e) => handleOtherNonRecurringContribution(e)}
+                    />
+                    <TextField
+                        name="other-recurring-contribution"
+                        label="Other Recurring Contribution (Total)"
+                        helperText="Other Recurring Contribution ($)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    $
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={otherRecurringContribution}
+                        onChange={(e) => handleOtherRecurringContribution(e)}
+                    />
+                    <TextField
+                        name="contribution-growth-rate"
+                        label="Contribution Growth Rate"
+                        helperText="Contribution growth rate (percentage)"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    %
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={contributionGrowthRate}
+                        onChange={(e) => handleContributionGrowthRate(e)}
                     />
                     <TextField
                         name="tenant-discount-rate"
@@ -153,7 +317,7 @@ const DealInputForm = ({
                     />
                     <TextField
                         name="commission-percent"
-                        label="Commission percent"
+                        label="Commission percent (Years  1 - 5)"
                         helperText="The commission percent"
                         type="number"
                         variant="standard"
@@ -164,8 +328,24 @@ const DealInputForm = ({
                                 </InputAdornment>
                             ),
                         }}
-                        value={commissionPercent}
-                        onChange={(e) => handleCommissionPercent(e)}
+                        value={commissionPercent1st}
+                        onChange={(e) => handleCommissionPercent1st(e)}
+                    />
+                    <TextField
+                        name="commission-percent"
+                        label="Commission percent (Years  6 - 10)"
+                        helperText="The commission percent"
+                        type="number"
+                        variant="standard"
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    %
+                                </InputAdornment>
+                            ),
+                        }}
+                        value={commissionPercent2nd}
+                        onChange={(e) => handleCommissionPercent2nd(e)}
                     />
                     <Button
                         variant="contained"
