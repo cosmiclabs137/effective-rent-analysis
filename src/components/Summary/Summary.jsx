01439a2c4e53aa9df2b87042477387ece09f1d85
jay ess <mentoce@gmail.com>
2024-06-02 20:42:01 -0700

    Update names to reflect new terminology

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import { Box } from "@mui/material";
 import SummaryTable from "../Tables/SummaryTable";
 
-const Summary = ({ ownerDeals, tenantDeals }) => {
+const Summary = ({ landlordDeals, tenantDeals }) => {
     return (
         <Box md={5}>
             <SummaryTable deals={tenantDeals} title="Tenant" />
-            <SummaryTable deals={ownerDeals} title="Owner" />
+            <SummaryTable deals={landlordDeals} title="Landlord" />
         </Box>
     );
 };
