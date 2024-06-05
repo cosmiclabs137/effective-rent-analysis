98e84cff0cc528d0d64956418372076fbeab40ca
jay ess <mentoce@gmail.com>
2024-06-05 16:39:46 -0700

    Remove unused imports/code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 import { Grid } from "@mui/material";
 
-import BasicHeader from "./components/Headers/BasicHeader";
-
 import "./App.css";
 
 function App() {
-    const [title, setTitle] = useState(null);
-    const location = useLocation();
-
-    useEffect(() => {
-        const parsedTitle = location.pathname.replace(/\W/g, " ");
-        setTitle(parsedTitle);
-    }, [location]);
     return (
         <React.Fragment>
             <CssBaseline />
             <Grid container>
-                {/* <BasicHeader title={title} /> */}
                 <Outlet />
             </Grid>
         </React.Fragment>
