4588aba55140839cbcdc43d080bb8dfef8b9fc0f
jay ess <mentoce@gmail.com>
2024-06-04 20:42:33 -0700

    Implement deals tables container that tabs between different deal calculations

---
+++ src/components/DealTablesContainer/DealTablesContainer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { Box, Tab, Tabs } from "@mui/material";
+
+import DealTable from "../DealTable/DealTable";
+import { columns } from "../../constants/columns";
+
+const DealPanel = ({ children, value, index }) =>
+    value === index && <Box>{children}</Box>;
+
+const DealTablesContainer = ({ deals }) => {
+    const [currentTabIndex, setCurrentTabIndex] = React.useState(0);
+
+    const handleTabChange = (e, tabIndex) => {
+        console.log("clicked tab!");
+        setCurrentTabIndex(tabIndex);
+    };
+
+    console.table(deals);
+
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                backgroundColor: "#f8f8ff",
+                minHeight: "90vh",
+                padding: 0,
+            }}
+        >
+            <Tabs
+                onChange={handleTabChange}
+                value={currentTabIndex}
+                allowScrollButtonsMobile
+                centered
+            >
+                {deals.map((deal, index) => (
+                    <Tab label={deal[0].name} key={index} wrapped />
+                ))}
+            </Tabs>
+            {deals.map((deal, index) => (
+                <DealPanel key={index} value={currentTabIndex} index={index}>
+                    <DealTable rows={deal} columns={columns} />
+                </DealPanel>
+            ))}
+        </Box>
+    );
+};
+
+export default DealTablesContainer;
