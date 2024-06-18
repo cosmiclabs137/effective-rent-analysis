6406206fff0f11db6c3c38c8c3e49106646ad17a
cosmiclabs137 <jayess137@proton.me>
2024-06-17 22:19:09 -0700

    Add custom range function and start work on function to calculate monthly payments

--- src/finance.ts
+++ src/finance.ts
@@ -1,3 +1,21 @@
-export function range(start: number, step: number, stop: number): number[] {
-    return [...Array(end).keys()];
+export function range(
+    stop: number,
+    start: number = 0,
+    step: number = 1
+): number[] {
+    const arr: number[] = [];
+
+    for (let i = start; i < stop; i += step) {
+        arr.push(i);
+    }
+
+    return arr;
+}
+
+export function calcMonthlyPayments(
+    period: number[],
+    baseRent: number,
+    sqft: number
+): number[] {
+    return [];
 }
